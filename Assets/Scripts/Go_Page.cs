0f328b035016ef5d6797e4123257c6bd0c45a03a
qjatjs123123 <74814641+qjatjs123123@users.noreply.github.com>
2021-05-23 20:51:46 +0900

    메뉴화면
    안녕

---
+++ Assets/Scripts/Go_Page.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Go_Page : MonoBehaviour
+{
+    public void go_page()
+    {
+        SceneManager.LoadScene("Round1");
+
+    }
+}
