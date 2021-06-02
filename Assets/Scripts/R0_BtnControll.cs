2329272bd118ecf6938fd898fc45ab6fb920a094
jinsu7500 <bluesky3213489@gmail.com>
2021-06-02 15:14:22 +0900

    Start_menu 음악추가

---
+++ Assets/Scripts/R0_BtnControll.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class R0_BtnControll : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public GameObject menu_Image; // 메뉴이미지
+
+    public void HoverSound()
+    {
+        myFx.PlayOneShot(hoverFx);
+    }
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Round1");
+    }
+    public void ExitBtn()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnBtn()
+    {
+        menu_Image.SetActive(false);        
+
+        //커서잠금해제    
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void RestartBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GotoTitleBtn()
+    {
+        SceneManager.LoadScene("Start_Menu");
+    }
+
+
+}
